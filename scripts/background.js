fd3af1a39ac7887e1aa990fe3366b2b2dd2e3856
keegang6705 <darunphobwi@gmail.com>
2024-06-16 12:14:14 +0700

    1.4

--- scripts/background.js
+++ scripts/background.js
@@ -1,2 +1,26 @@
 console.log("MySkEnchanter/scripts/background.js:LOADED")
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    let tabIndex = tab.index;
+    if (changeInfo.status == "complete" && tab.url == "https://www.mysk.school/classes") {
+        chrome.tabs.remove(tabId);
+        chrome.tabs.create({ url: `https://www.mysk.school/classes?ske=true&ske_tab_index=${tab.index}` });
+    } else if (changeInfo.status == "complete" && tab.url.startsWith("https://www.mysk.school/classes?ske=true")) {
+        const url = new URL(tab.url);
+        const decodedSearchParams = new URLSearchParams(url.searchParams);
+        const tabIndex = decodedSearchParams.get("ske_tab_index");
+        moveToFirstPosition(tabId,parseInt(tabIndex))
+    }
+  });
+  
 
+  async function moveToFirstPosition(tabId,index) {
+    try {
+      await chrome.tabs.move(tabId, {index: index});
+    } catch (error) {
+      if (error == "Error: Tabs cannot be edited right now (user may be dragging a tab).") {
+        setTimeout(() => moveToFirstPosition(tabId), 50);
+      } else {
+        console.error(error);
+      }
+    }
+  }
--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,4 @@
-console.log("MySkEnchanter/scripts/content.js:LOADED")
+console.log("MySkEnchanter/scripts/content.js:LOADED");
 const api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrcXFlcGJvZHFqaGl3ZmpjdnhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA3ODQ3NDQsImV4cCI6MTk2NjM2MDc0NH0.AuMiPwZfjapD_EW8ZyEWq9y6z3JqLC_rO3V4Bw7K300";
 headers = {
   'Authorization': `Bearer ${api_key}`,
@@ -15,16 +15,89 @@ async function getClassroomInfo(classroom_id,extension=null){
     const url = extension? `https://ykqqepbodqjhiwfjcvxe.supabase.co/rest/v1/classrooms?select=${extension}&id=${classroom_id}&limit=1&order=id.asc&apikey=${api_key}`:`https://ykqqepbodqjhiwfjcvxe.supabase.co/rest/v1/classrooms?select=number,classroom_students(class_no)&classroom_students.order=class_no.asc&id=${classroom_id}&limit=1&order=id.asc&apikey=${api_key}`;
     const response = await fetch(url);
   if (response){
-    data = response.json();
-    return data? data[0]["number"]:null;
+    data = await response.json();
+    console.log(`MySkEnchanter/scripts/content.js:FETCH ${url}`);
+    return await data? data[0]:"ERR";
   } else {
-    console.log(`Error retrieving data for ${classroom_id} : ${response.status_code}`);
-    return None;
+    console.error(`MySkEnchanter/scripts/content.js:ERROR retrieving data for ${classroom_id}`);
+    return "ERR";
   }
 }
 async function main(){
     const class_id = await loadJson();
-    alert(JSON.stringify(class_id))
+    const class_info = await getClassroomInfo( class_id["408"],"id%2Cnumber%2Cmain_room%2Cclassroom_advisors%21inner%28teachers%21inner%28id%2Csubject_groups%28id%2Cname_en%2Cname_th%29%2Cpeople%28first_name_en%2Cfirst_name_th%2Cmiddle_name_en%2Cmiddle_name_th%2Clast_name_en%2Clast_name_th%2Cprofile%29%29%29%2Cclassroom_students%28class_no%2Cstudents%21inner%28id%2Cpeople%21inner%28first_name_en%2Cfirst_name_th%2Cmiddle_name_en%2Cmiddle_name_th%2Clast_name_en%2Clast_name_th%2Cnickname_en%2Cnickname_th%2Cprofile%29%29%29%2Cclassroom_contacts%28contacts%21inner%28*%29%29%2Cyear&classroom_students.order=class_no.asc");
 };
 main()
 
+
+
+
+    
+
+  async function listener () {
+    const targetElement = document.querySelector('main');
+    if (targetElement) {
+      const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+          if(mutation["addedNodes"][0]){
+            let class_number = mutation["addedNodes"][0]
+            console.log(class_number);
+          };
+        });
+      });
+      console.log("MySkEnchanter/scripts/content.js: INFO Obeserving main")
+      observer.observe(targetElement, { childList: true,attributes: true });
+    } else {
+      console.log('Target element not found!');
+    }
+  }
+  
+  window.addEventListener('load', function load(e) {
+    window.removeEventListener('load', load, false);
+  
+    const overlay = document.createElement('div');
+    overlay.id = 'loading-overlay';
+    overlay.style.cssText = `
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.8);
+      z-index: 9999;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    `;
+  
+    const loadingIndicator = document.createElement('div');
+    loadingIndicator.id = 'loading-indicator';
+    loadingIndicator.style.cssText = `
+      width: 100px;
+      height: 100px;
+      border: 10px solid #fff;
+      border-radius: 50%;
+      border-top: 10px solid #7ff777;
+      animation: spin 2s linear infinite;
+    `;
+  
+    const animationKeyframes = `
+      @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+      }
+    `;
+  
+    overlay.appendChild(loadingIndicator);
+    document.body.appendChild(overlay);
+  
+    setTimeout(() => {
+      overlay.style.display = 'none';
+      listener()
+    }, 2000);
+    const styleElement = document.createElement('style');
+    styleElement.textContent = animationKeyframes;
+    document.head.appendChild(styleElement);
+  }, false);
+
+  
