58d8659cb57c0d5511f1d95fd729bcca981fc7de
keegang6705 <darunphobwi@gmail.com>
2024-08-04 14:18:26 +0700

    add ske=false if not match

--- scripts/background.js
+++ scripts/background.js
@@ -1,20 +1,33 @@
 console.log("MySkEnchanter/scripts/background.js:LOADED");
+
 const url_list = [
   { path: "classes", parameter: "" },
   { path: "search/students/results", parameter: "" },
 ];
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "loading" &&tab.url.startsWith("https://www.mysk.school")) {
+  if (changeInfo.status === "loading" && tab.url.startsWith("https://www.mysk.school")) {
+    let foundMatch = false;
+    const fill = tab.url.includes("?") ? "&" : "?";
     for (let i = 0; i < url_list.length; i++) {
       const current_url = url_list[i].path;
       const current_parameter = url_list[i].parameter;
-      const fill = tab.url.includes("?") ? "&" : "?";
+      
       if (tab.url.startsWith(`https://www.mysk.school/${current_url}`) || tab.url.startsWith(`https://www.mysk.school/en-US/${current_url}`)) {
-        if (tab.url.includes("ske=true")) {
+        foundMatch = true;
+        
+        if (tab.url.includes("ske=true") || tab.url.includes("ske=false")) {
           break;
         } else {
-            chrome.tabs.update(tabId, { url: `${tab.url}${fill}ske=true${current_parameter}` });
+          chrome.tabs.update(tabId, { url: `${tab.url}${fill}ske=true${current_parameter}` });
         }
+        break;
+      }
+    }
+    
+    if (!foundMatch) {
+      if (!tab.url.includes("ske=")) {
+        chrome.tabs.update(tabId, { url: `${tab.url}${fill}ske=false` });
       }
     }
   }
